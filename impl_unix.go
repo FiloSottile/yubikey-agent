faee8482e896f845bbc299fecabb5ad6b71eb6fb
Axel Gluth <axel.gluth@innogames.com>
2020-12-22 07:01:44 +0100

    Add missing license headers

--- impl_unix.go
+++ impl_unix.go
@@ -1,5 +1,11 @@
 // +build darwin dragonfly freebsd linux netbsd openbsd
 
+// Copyright 2020 Google LLC
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://developers.google.com/open-source/licenses/bsd
+
 package main
 
 import (
